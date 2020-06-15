c0a4d6c6843218265e575870f4ad7e85566f0f03
Andrew Sidlo <asidlo@nexidia.com>
2020-06-15 09:40:29 -0400

    Added powershell jdtls start script

---
+++ vim/java/bin/jdtls.ps1
@@ -0,0 +1,31 @@
+$jdtlsTargetRepo = "$env:HOME\vimfiles\java\eclipse.jdt.ls\org.eclipse.jdt.ls.product\target\repository"
+$jarFile = Get-ChildItem $jdtlsTargetRepo\plugins | Where-Object Name -Match org.eclipse.equinox.launcher_.*
+
+# JDK9+
+# java `
+#   '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044' `
+#   '-Declipse.application=org.eclipse.jdt.ls.core.id2' `
+#   '-Dosgi.bundles.defaultStartLevel=4' `
+#   '-Declipse.product=org.eclipse.jdt.ls.core.product' `
+#   '-Dlog.protocol=true' `
+#   '-Dlog.level=ALL' `
+#   '-noverify' `
+#   '-Xmx4G' `
+#   '-jar' "$($jarFile.FullName)" `
+#   '-configuration' "$jdtlsTargetRepo\config_win" `
+#   '-data' "$env:APPDATA\eclipse" `
+#   '--add-modules=ALL-SYSTEM' `
+#   '--add-opens java.base/java.util=ALL-UNNAMED' `
+#   '--add-opens java.base/java.lang=ALL-UNNAMED'
+
+java `
+  '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=1044' `
+  '-Declipse.application=org.eclipse.jdt.ls.core.id1' `
+  '-Dosgi.bundles.defaultStartLevel=4' `
+  '-Declipse.product=org.eclipse.jdt.ls.core.product' `
+  '-Dlog.level=ALL' `
+  '-noverify' `
+  '-Xmx4G' `
+  '-jar' "$($jarFile.FullName)" `
+  '-configuration' "$jdtlsTargetRepo\config_win" `
+  '-data' "$env:APPDATA\eclipse" `
