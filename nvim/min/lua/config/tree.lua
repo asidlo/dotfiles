02faae21e28bd9909fb96a81b262bf78f647005c
Andrew Sidlo <asidlo@nexidia.com>
2021-07-29 10:14:30 -0400

    Fixed auto resize issue with nvim tree

--- nvim/min/lua/config/tree.lua
+++ nvim/min/lua/config/tree.lua
@@ -1,7 +1,7 @@
 set_keymap('n', '<Leader>n', '<Cmd>NvimTreeToggle<CR>')
 set_keymap('n', '<Leader>N', '<Cmd>NvimTreeFindFile<CR>')
 vim.g.nvim_tree_width = 40
-vim.g.nvim_tree_width_allow_resize = 1
+vim.g.nvim_tree_auto_resize = 0
 vim.g.nvim_tree_window_picker_exclude = {
 	filetype = {
 		'packer',
