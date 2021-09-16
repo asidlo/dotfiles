c40158d092d71eee8a054018573f984b8ce14f44
asidlo <sidlo.andrew@gmail.com>
2021-09-15 21:39:29 -0400

    Formatted treesitter.lua

--- nvim/config/lua/config/treesitter.lua
+++ nvim/config/lua/config/treesitter.lua
@@ -1,69 +1,32 @@
 local ts = require('nvim-treesitter.configs')
 
 local plugins = {
-	'bash',
-	'java',
-	'lua',
-	'rust',
-	'toml',
-	'go',
-	'gomod',
-	'json',
-	'jsonc',
-	'python',
-	'dockerfile',
-	'cpp',
-	'c',
-	'regex'
+    'bash', 'java', 'lua', 'rust', 'toml', 'go', 'gomod', 'json', 'jsonc', 'python', 'rst', 'dockerfile', 'cpp', 'c',
+    'cmake', 'regex', 'yaml'
 }
 
 ts.setup {
-	ensure_installed = plugins,
-	highlight = {
-		enable = true
-	},
-	incrementalSelection = {
-		enable = true
-	},
-	indent = {
-		enable = true,
-		disable = {'java', 'json'}
-	},
-	textobjects = {
-		select = {
-			enable = true,
-			keymaps = {
-				['af'] = '@function.outer',
-				['if'] = '@function.inner',
-				['ac'] = '@class.outer',
-				['ic'] = '@class.inner'
-			}
-		},
-		lsp_interop = {
-			enable = true,
-			peek_definition_code = {
-				['gp'] = '@function.outer',
-				['gP'] = '@class.outer'
-			}
-		},
-		move = {
-			enable = true,
-			goto_next_start = {
-				[']m'] = '@function.outer',
-				[']]'] = '@class.outer'
-			},
-			goto_next_end = {
-				[']M'] = '@function.outer',
-				[']['] = '@class.outer'
-			},
-			goto_previous_start = {
-				['[m'] = '@function.outer',
-				['[['] = '@class.outer'
-			},
-			goto_previous_end = {
-				['[M'] = '@function.outer',
-				['[]'] = '@class.outer'
-			}
-		}
-	}
+    ensure_installed = plugins,
+    highlight = {enable = true},
+    incrementalSelection = {enable = true},
+    indent = {enable = true, disable = {'java', 'json'}},
+    textobjects = {
+        select = {
+            enable = true,
+            keymaps = {
+                ['af'] = '@function.outer',
+                ['if'] = '@function.inner',
+                ['ac'] = '@class.outer',
+                ['ic'] = '@class.inner'
+            }
+        },
+        lsp_interop = {enable = true, peek_definition_code = {['gp'] = '@function.outer', ['gP'] = '@class.outer'}},
+        move = {
+            enable = true,
+            goto_next_start = {[']m'] = '@function.outer', [']]'] = '@class.outer'},
+            goto_next_end = {[']M'] = '@function.outer', [']['] = '@class.outer'},
+            goto_previous_start = {['[m'] = '@function.outer', ['[['] = '@class.outer'},
+            goto_previous_end = {['[M'] = '@function.outer', ['[]'] = '@class.outer'}
+        }
+    }
 }
