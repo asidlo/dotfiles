3ab1fee5323035d11508613924208c656367d5c8
asidlo <sidlo.andrew@gmail.com>
2021-12-21 12:41:22 -0500

    Added ui-select, formatted, and C-K for i+s

--- nvim/fromscratch/lua/user/autocommands.lua
+++ nvim/fromscratch/lua/user/autocommands.lua
@@ -1,38 +1,37 @@
 vim.cmd([[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo,null-ls-info nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200}) 
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-  augroup end
+    augroup _general_settings
+        autocmd!
+        autocmd FileType qf,help,man,lspinfo,null-ls-info nnoremap <silent> <buffer> q :close<CR> 
+        autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200}) 
+        autocmd BufWinEnter * :set formatoptions-=cro
+        autocmd FileType qf set nobuflisted
+    augroup end
 
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
+    augroup _git
+        autocmd!
+        autocmd FileType gitcommit setlocal wrap
+        autocmd FileType gitcommit setlocal spell
+    augroup end
 
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
+    augroup _markdown
+        autocmd!
+        autocmd FileType markdown setlocal wrap
+        autocmd FileType markdown setlocal spell
+    augroup end
 
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
+    augroup _auto_resize
+        autocmd!
+        autocmd VimResized * tabdo wincmd = 
+    augroup end
 
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
+    augroup _alpha
+        autocmd!
+        autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+    augroup end
 
-  augroup _terminal
-    autocmd!
-    autocmd TermOpen,TermEnter term://* startinsert!
-    autocmd TermEnter term://* setlocal nonumber norelativenumber signcolumn=no
-  augroup end
-
+    augroup _terminal
+        autocmd!
+        autocmd TermOpen,TermEnter term://* startinsert!
+        autocmd TermEnter term://* setlocal nonumber norelativenumber signcolumn=no
+    augroup end
 ]])
--- nvim/fromscratch/lua/user/keymaps.lua
+++ nvim/fromscratch/lua/user/keymaps.lua
@@ -20,10 +20,10 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- keymap("n", "<C-h>", "<C-w>h", opts)
+-- keymap("n", "<C-j>", "<C-w>j", opts)
+-- keymap("n", "<C-k>", "<C-w>k", opts)
+-- keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
@@ -32,8 +32,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==", opts)
