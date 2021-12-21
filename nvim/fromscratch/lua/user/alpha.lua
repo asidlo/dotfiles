a077c9c15f96e9e990e729bac0c26e35a869374e
asidlo <sidlo.andrew@gmail.com>
2021-12-21 15:22:20 -0500

    Updates alpha logo and footer

--- nvim/fromscratch/lua/user/alpha.lua
+++ nvim/fromscratch/lua/user/alpha.lua
@@ -1,41 +1,43 @@
-local status_ok, alpha = pcall(require, "alpha")
+local status_ok, alpha = pcall(require, 'alpha')
 if not status_ok then
-	return
+    return
 end
 
-local dashboard = require("alpha.themes.dashboard")
+local dashboard = require('alpha.themes.dashboard')
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    '                                                     ',
+    '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+    '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+    '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+    '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+    '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+    '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+    '                                                     ',
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
+    dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
+    dashboard.button('p', '  Find project', ':Telescope projects <CR>'),
+    dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
+    dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
+    dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua <CR>'),
+    dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
 }
 
 -- local function footer()
--- -- NOTE: requires the fortune-mod package to work
--- 	-- local handle = io.popen("fortune")
--- 	-- local fortune = handle:read("*a")
--- 	-- handle:close()
--- 	-- return fortune
--- 	return "chrisatmachine.com"
+--     local total_plugins = #vim.fn.globpath(vim.fn.stdpath('data') .. '/site/pack/packer/start', '*', 0, 1)
+--     local datetime = os.date('%d-%m-%Y %H:%M:%S')
+--     return string.format('Loaded %d plugins   on  %s', total_plugins, datetime)
 -- end
 
--- dashboard.section.footer.val = footer()
+-- local fortune = require('alpha.fortune')
+-- dashboard.section.footer.val = fortune
+dashboard.section.footer.val = os.date(' %d-%m-%Y   %H:%M:%S')
 
+dashboard.section.header.opts.hl = 'Include'
 dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
+-- dashboard.section.footer.opts.hl = 'Keyword'
 
 dashboard.opts.opts.noautocmd = true
 -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
