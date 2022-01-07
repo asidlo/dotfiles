ae8647284743abd504eea1f033f7919a9b350267
asidlo <sidlo.andrew@gmail.com>
2022-01-07 13:27:08 -0500

    Refactors statusline to be more vscody

--- nvim/fromscratch/lua/user/lualine.lua
+++ nvim/fromscratch/lua/user/lualine.lua
@@ -1,84 +1,72 @@
-local status_ok, lualine = pcall(require, "lualine")
+local status_ok, lualine = pcall(require, 'lualine')
 if not status_ok then
-	return
+    return
 end
 
 local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
+    return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
-	colored = true,
-	update_in_insert = false,
-	always_visible = true,
+    'diagnostics',
+    sources = { 'nvim_diagnostic' },
+    sections = { 'error', 'warn' },
+    symbols = { error = ' ', warn = ' ' },
+    colored = true,
+    update_in_insert = false,
+    always_visible = true,
 }
 
 local diff = {
-	"diff",
-	colored = true,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-	cond = hide_in_width,
+    'diff',
+    colored = true,
+    symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+    cond = hide_in_width,
 }
 
 local mode = {
-	"mode",
-	fmt = function(str)
-		return "-- " .. str .. " --"
-	end,
-}
-
-local filetype = {
-	"filetype",
-	icons_enabled = true,
-	-- icon = nil,
+    'mode',
+    fmt = function(str)
+        return '-- ' .. str .. ' --'
+    end,
 }
 
 local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
-}
-
-local location = {
-	"location",
-	padding = 0,
+    'branch',
+    icons_enabled = true,
+    icon = '',
 }
 
 local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+    return 'spaces: ' .. vim.api.nvim_buf_get_option(0, 'shiftwidth')
 end
 
-
 lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {branch, diagnostics, mode},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = {diff, spaces, "encoding", filetype, location  },
-    lualine_y = {},
-		lualine_z = {},
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        disabled_filetypes = { 'alpha', 'dashboard', 'NvimTree', 'Outline' },
+        always_divide_middle = true,
+    },
+    sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { branch, diagnostics, mode },
+        -- lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = { diff, 'location', spaces, 'encoding', 'fileformat', 'filetype' },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    extensions = {},
 })
