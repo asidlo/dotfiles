a7aa3bfb10e0baa38cc816f5ad664667873a10c6
asidlo <sidlo.andrew@gmail.com>
2022-01-10 09:26:44 -0500

    Shows unique tabs names

--- nvim/fromscratch/lua/user/bufferline.lua
+++ nvim/fromscratch/lua/user/bufferline.lua
@@ -64,7 +64,7 @@ bufferline.setup {
     -- can also be a table containing 2 custom separators
     -- [focused and unfocused]. eg: { '|', '|' }
     separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
-    enforce_regular_tabs = true,
+    enforce_regular_tabs = false,
     always_show_bufferline = true,
     -- sort_by = 'id' | 'extension' | 'relative_directory' | 'directory' | 'tabs' | function(buffer_a, buffer_b)
     --   -- add custom logic
@@ -119,8 +119,8 @@ bufferline.setup {
     },
 
     duplicate_selected = {
-      guifg = { attribute = "fg", highlight = "TabLineSel" },
-      guibg = { attribute = "bg", highlight = "TabLineSel" },
+      -- guifg = { attribute = "fg", highlight = "Normal" },
+      -- guibg = { attribute = "bg", highlight = "Normal" },
       gui = "italic",
     },
     duplicate_visible = {
