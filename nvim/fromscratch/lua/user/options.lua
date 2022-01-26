a1d27e5106a5e281b3b92db6e511fd79d3428e79
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-01-26 15:07:34 -0500

    Removed issue with monospace

--- nvim/fromscratch/lua/user/options.lua
+++ nvim/fromscratch/lua/user/options.lua
@@ -30,7 +30,6 @@ local options = {
     wrap = false, -- display lines as one long line
     -- scrolloff = 8, -- is one of my fav
     -- sidescrolloff = 8,
-    guifont = 'monospace:h17', -- the font used in graphical neovim applications
     wildmode = { 'longest:full', 'full' },
     completeopt = { 'menu', 'menuone', 'noselect' },
     foldenable = false,
