2e6c1071dc32dce8d7963276d2749e8e1509da7e
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-02-18 09:58:18 -0500

    lldb-vscode cross platform

--- nvim/fromscratch/lua/user/dap/lldb-vscode.lua
+++ nvim/fromscratch/lua/user/dap/lldb-vscode.lua
@@ -3,38 +3,44 @@ if not dap_ok then
     return
 end
 
+local exe
+if vim.fn.has('win32') == 1 then
+    exe = [[ C:\Program Files\LLVM\bin\lldb-vscode.exe ]]
+else
+    exe = '/usr/local/bin/lldb-vscode'
+end
+
 dap.adapters.lldb = {
     type = 'executable',
-    command = '/usr/local/bin/lldb-vscode', -- adjust as needed
+    command = exe,
     name = 'lldb',
 }
 
 dap.configurations.cpp = {
-  {
-    name = "Launch",
-    type = "lldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
+    {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
 
-    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-    --
-    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --
-    -- Otherwise you might get the following error:
-    --
-    --    Error on launch: Failed to attach to the target process
-    --
-    -- But you should be aware of the implications:
-    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-    runInTerminal = false,
-  },
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        runInTerminal = false,
+    },
 }
-
 
 -- If you want to use this for rust and c, add something like this:
 
