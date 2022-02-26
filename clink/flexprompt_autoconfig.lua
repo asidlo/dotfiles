2d308de88f95ba3f8a6f4131d74ac096d72d11f3
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-02-25 22:44:43 -0500

    Copies all clink files

---
+++ clink/flexprompt_autoconfig.lua
@@ -0,0 +1,27 @@
+-- WARNING:  This file gets overwritten by the 'flexprompt configure' wizard!
+--
+-- If you want to make changes, consider copying the file to
+-- 'flexprompt_config.lua' and editing that file instead.
+
+flexprompt.settings.connection = "disconnected"
+flexprompt.settings.lines = "two"
+flexprompt.settings.left_frame = "none"
+flexprompt.settings.flow = "fluent"
+flexprompt.settings.left_prompt = "{battery}{cwd}{git}{duration}"
+flexprompt.settings.symbols =
+{
+    prompt =
+    {
+        ">",
+        winterminal = "❯",
+    },
+}
+flexprompt.settings.charset = "unicode"
+flexprompt.settings.use_icons = true
+flexprompt.settings.powerline_font = true
+flexprompt.settings.heads = "pointed"
+flexprompt.settings.style = "lean"
+flexprompt.settings.spacing = "compact"
+flexprompt.settings.right_frame = "none"
+flexprompt.settings.lean_separators = "space"
+flexprompt.settings.use_8bit_color = true
