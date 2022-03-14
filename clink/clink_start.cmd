e1280831afc92ef4a9d332ede373374ccdfe0e5a
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-03-14 13:31:01 -0400

    Fixes pwsh vs cmd fzf null

--- clink/clink_start.cmd
+++ clink/clink_start.cmd
@@ -4,3 +4,7 @@ doskey ll=exa -l $*
 doskey cat=bat --style plain --color never $*
 doskey jq=jq -C $*
 doskey pwd=echo %cd%
+doskey k=kubectl $*
+
+set FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard 2>nul | fd --type f --type l %FD_OPTS%"
+set FZF_CTRL_T_COMMAND=%FZF_DEFAULT_COMMAND%
