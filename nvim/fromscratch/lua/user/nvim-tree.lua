bcdcbf3c41f8b6fe0bdcf939b3e327cf3926c346
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-04-27 16:17:44 +0000

    Removes outdated config options

--- nvim/fromscratch/lua/user/nvim-tree.lua
+++ nvim/fromscratch/lua/user/nvim-tree.lua
@@ -44,14 +44,14 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
-  auto_close = false,
+  -- auto_close = false,
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
+  -- update_to_buf_dir = {
+  --   enable = true,
+  --   auto_open = true,
+  -- },
   diagnostics = {
     enable = true,
     icons = {
@@ -79,12 +79,21 @@ nvim_tree.setup {
     ignore = true,
     timeout = 500,
   },
+    actions = {
+        change_dir = {
+            enable = true
+        },
+        open_file = {
+            quit_on_open = false,
+            resize_window = false
+        }
+    },
   view = {
     width = 30,
     height = 30,
     hide_root_folder = false,
     side = "left",
-    auto_resize = false,
+    -- auto_resize = false,
     mappings = {
       custom_only = false,
       list = {
@@ -100,15 +109,15 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
-  },
+  -- quit_on_open = 0,
+  -- git_hl = 1,
+  -- disable_window_picker = 0,
+  -- root_folder_modifier = ":t",
+  -- show_icons = {
+  --   git = 1,
+  --   folders = 1,
+  --   files = 1,
+  --   folder_arrows = 1,
+  --   tree_width = 30,
+  -- },
 }
