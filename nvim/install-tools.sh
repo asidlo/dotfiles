7c122431ab2c6d4137548dae44bf376fa852fe5e
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-06-01 12:13:10 -0400

    Installs antibody

--- nvim/install-tools.sh
+++ nvim/install-tools.sh
@@ -35,8 +35,12 @@ mkdir -p ~/.config
 ln -svf ~/.local/src/dotfiles/zsh/starship.toml ~/.config/starship.toml
 ln -svf ~/.local/src/dotfiles/nvim/fromscratch ~/.config/nvim
 
-# Run zsh login shell to install antibody plugins
-zsh
+mkdir -p ~/.local/bin
+curl -sfL git.io/antibody | sh -s - -b ~/.local/bin
+source <(~/.local/bin/antibody init)
+~/.local/bin/antibody bundle zsh-users/zsh-syntax-highlighting
+~/.local/bin/antibody bundle zsh-users/zsh-history-substring-search
+~/.local/bin/antibody zsh-users/zsh-autosuggestions
 
 # Install nvim
 ~/.local/src/dotfiles/nvim/download-stable-nvim-local.sh
