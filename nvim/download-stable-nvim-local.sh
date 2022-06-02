e4acf02864531f7a28c68f4eaf4c125f4ea51ef0
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-06-02 14:42:30 -0400

    Fixes mv -f mistake

--- nvim/download-stable-nvim-local.sh
+++ nvim/download-stable-nvim-local.sh
@@ -12,6 +12,9 @@ pushd ~/.local
 
 ./nvim.appimage --appimage-extract 
 
+if [ -d ~/.local/nvim ]; then
+    rm -rf ~/.local/nvim
+fi
 mv -f ./squashfs-root ~/.local/nvim
 
 ln -svf ~/.local/nvim/AppRun ~/.local/bin/nvim
