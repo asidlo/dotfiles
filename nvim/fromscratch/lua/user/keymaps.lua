53708f04a0dff585f1f5470a5bddf56447016c6a
Andrew Sidlo <andrewsidlo@microsoft.com>
2022-07-14 15:18:05 -0400

    Removes uncessary hydra bindings

--- nvim/fromscratch/lua/user/keymaps.lua
+++ nvim/fromscratch/lua/user/keymaps.lua
@@ -20,10 +20,13 @@ vim.g.maplocalleader = ' '
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- keymap("n", "<C-h>", "<C-w>h", opts)
+-- keymap("n", "<C-j>", "<C-w>j", opts)
+-- keymap("n", "<C-k>", "<C-\\><C-N><C-w>k", {})
+-- keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Ensure C-c acts like escape
+keymap('n', '<C-c>', '<Esc>', {})
 
 keymap('n', 'Y', 'y$', opts)
 keymap('n', 'n', 'nzzzv', opts)
@@ -77,10 +80,10 @@ keymap('x', '<A-k>', ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- TODO (AS): Create a custom code-action for vale to add to dictionary
 -- could also add the top 10 entries in z=
