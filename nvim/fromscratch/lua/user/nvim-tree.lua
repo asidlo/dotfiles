c3e7f5b762f15155c49fa58a164686a0083f11e3
Andrew Sidlo <andrewsidlo@microsoft.com>
2023-02-27 10:19:20 -0500

    Addresses Nvim tree toggle not showing

--- nvim/fromscratch/lua/user/nvim-tree.lua
+++ nvim/fromscratch/lua/user/nvim-tree.lua
@@ -6,12 +6,12 @@ if not status_ok then
     return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, 'nvim-tree.config')
-if not config_status_ok then
-    return
-end
+-- local config_status_ok, nvim_tree_config = pcall(require, 'nvim-tree.config')
+-- if not config_status_ok then
+--     return
+-- end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+-- local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
     disable_netrw = true,
@@ -95,14 +95,14 @@ nvim_tree.setup({
         hide_root_folder = false,
         side = 'left',
         -- auto_resize = false,
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = { 'l', '<CR>', 'o' }, cb = tree_cb('edit') },
-                { key = 'h', cb = tree_cb('close_node') },
-                { key = 'v', cb = tree_cb('vsplit') },
-            },
-        },
+        -- mappings = {
+        --     custom_only = false,
+        --     list = {
+        --         { key = { 'l', '<CR>', 'o' }, cb = tree_cb('edit') },
+        --         { key = 'h', cb = tree_cb('close_node') },
+        --         { key = 'v', cb = tree_cb('vsplit') },
+        --     },
+        -- },
         number = false,
         relativenumber = false,
     },
