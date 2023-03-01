b878711262e40edf914c707c129fe98f00b422e7
Andrew Sidlo <andrewsidlo@microsoft.com>
2023-03-01 21:05:10 +0000

    Fixes json lsp schemastore overrides

--- nvim/fromscratch/lua/user/lsp/settings/jsonls.lua
+++ nvim/fromscratch/lua/user/lsp/settings/jsonls.lua
@@ -1,40 +1,42 @@
-local default_schemas = nil
-local status_ok, jsonls_settings = pcall(require, 'nlspsettings.jsonls')
-if status_ok then
-    default_schemas = jsonls_settings.get_default_schemas()
-end
-
+-- local default_schemas = nil
+-- local status_ok, jsonls_settings = pcall(require, 'nlspsettings.jsonls')
+-- if status_ok then
+--     default_schemas = jsonls_settings.get_default_schemas()
+-- end
 local store_ok, store = pcall(require, 'schemastore')
 local store_schemas = nil
 if store_ok then
     store_schemas = store.json.schemas()
 end
 
-local local_schemas = {
-    -- {
-    --     description = 'Microsoft terminal configuration file',
-    --     fileMatch = { 'settings.json' },
-    --     url = 'https://aka.ms/terminal-profiles-schema',
-    -- },
-}
+-- local local_schemas = {
+--     -- {
+--     --     description = 'Microsoft terminal configuration file',
+--     --     fileMatch = { 'settings.json' },
+--     --     url = 'https://aka.ms/terminal-profiles-schema',
+--     -- },
+-- }
 
-local function extend(tab1, tab2)
-    if not tab2 then
-        return
-    end
-    for _, value in ipairs(tab2) do
-        table.insert(tab1, value)
-    end
-    return tab1
-end
+-- local function extend(tab1, tab2)
+--     if not tab2 then
+--         return
+--     end
+--     for _, value in ipairs(tab2) do
+--         table.insert(tab1, value)
+--     end
+--     return tab1
+-- end
 
-local extended_schemas = extend(store_schemas, default_schemas)
-extended_schemas = extend(local_schemas, extended_schemas)
+-- local extended_schemas = extend(store_schemas, default_schemas)
+-- extended_schemas = extend(local_schemas, extended_schemas)
 
 local opts = {
     settings = {
         json = {
-            schemas = extended_schemas,
+            schemas = store_schemas,
+            validate = {
+                enable = true
+            }
         },
     },
     setup = {
--- nvim/fromscratch/lua/user/mason-lspconfig.lua
+++ nvim/fromscratch/lua/user/mason-lspconfig.lua
@@ -2,19 +2,19 @@ local mason_present, mason_lspconfig = pcall(require, "mason-lspconfig")
 local lspconf_present, lspconfig = pcall(require, "lspconfig")
 
 if not mason_present or not lspconf_present then
-  return
+    return
 end
 
 mason_lspconfig.setup()
 
 mason_lspconfig.setup_handlers {
-  function (server_name)
-    local has_custom_opts, server_custom_opts = pcall(require, 'user.lsp.settings.' .. server_name)
-    local opts = lspconfig.util.default_config
-    if has_custom_opts then
-        opts = vim.tbl_deep_extend('force', server_custom_opts, opts)
+    function(server_name)
+        local has_custom_opts, server_custom_opts = pcall(require, 'user.lsp.settings.' .. server_name)
+        local opts = lspconfig.util.default_config
+        if has_custom_opts then
+            opts = vim.tbl_deep_extend('force', opts, server_custom_opts)
+        end
+        opts.on_attach = require('user.lsp.handlers').on_attach
+        lspconfig[server_name].setup(opts)
     end
-    opts.on_attach = require('user.lsp.handlers').on_attach
-    lspconfig[server_name].setup(opts)
-  end
 }
