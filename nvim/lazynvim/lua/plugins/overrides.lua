61796140c4a6ed27787e8408a8cceb70356263c3
asidlo <sidlo.andrew@gmail.com>
2024-03-08 21:27:13 -0500

    Adds linters and uses builtin go lazyvim config

--- nvim/lazynvim/lua/plugins/overrides.lua
+++ nvim/lazynvim/lua/plugins/overrides.lua
@@ -1,27 +1,47 @@
 return {
   {
     "echasnovski/mini.animate",
-    event = "VeryLazy",
     opts = function(_, opts)
       opts.cursor = { enable = false }
     end,
   },
   {
-    "williamboman/mason.nvim",
-
-    opts = {
-      ensure_installed = {
-        "codespell",
-        "bash-language-server",
-        "shfmt",
-      },
-    },
+    "nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "copilot",
+        group_index = 1,
+        priority = 1,
+      })
+    end,
   },
   {
     "mfussenegger/nvim-lint",
     opts = {
       linters_by_ft = {
+        javascript = { "eslint" },
+        javascriptreact = { "eslint" },
+        typescript = { "eslint" },
+        typescriptreact = { "eslint" },
+        bash = { "shellcheck" },
+        sh = { "shellcheck" },
+        gitcommit = { "gitlint" },
+        text = { "vale" },
+        markdown = { "markdownlint", "vale" },
         ["*"] = { "codespell" },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gitlint",
+        "eslint-lsp",
+        "shellcheck",
+        "codespell",
+        "markdownlint",
+        "vale",
       },
     },
   },
