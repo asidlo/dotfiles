7a4a3967a086e05b8476d1b281cab00fea2a1b63
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-03-14 15:00:37 +0000

    Readded symlink for fzf

--- scripts/fzf.sh
+++ scripts/fzf.sh
@@ -6,3 +6,4 @@ if [ ! -d ~/.local/src/fzf/ ]; then
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/src/fzf
 fi
 ~/.local/src/fzf/install --xdg --key-bindings --completion --no-update-rc --no-bash --no-fish
+ln -svf ~/.local/src/fzf/bin/fzf ~/.local/bin/fzf
