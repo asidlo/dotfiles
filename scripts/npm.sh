fe8268bfc110feb33c5fa23d662f2a45aa20cb18
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-06-04 15:52:41 -0400

    Fixes npm install script

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -5,9 +5,15 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | ba
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-# Removes decimal to use built in integer math instead of floats
-if (($(echo "$UBUNTU_VERSION" | sed 's/\.//g') > 1804)); then
-	nvm install --lts
+source /etc/os-release
+
+if [ "$ID" == "ubuntu" ]; then
+	# Removes decimal to use built in integer math instead of floats
+	if (($(echo "$VERSION_ID" | sed 's/\.//g') <= 1804)); then
+		nvm install 16.15.1
+	else
+		nvm install --lts
+	fi
 else
-	nvm install 16.15.1
+	nvm install --lts
 fi
