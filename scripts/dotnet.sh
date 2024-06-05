6884a4d5296c2e770a471755867984ce7465bf1f
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-06-05 11:43:45 -0400

    Fixes dotnet install script for ubuntu

--- scripts/dotnet.sh
+++ scripts/dotnet.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+# Get current script location
+DOTFILES_DIR=$(dirname "$(realpath "${BASH_SOURCE:-$0}")")
+DOTNET_VERSION="${DOTNET_VERSION:-8.0}"
+
 mkdir -p ~/.omnisharp
 ln -sfv "$DOTFILES_DIR/misc/omnisharp.json" ~/.omnisharp/omnisharp.json
+
+source /etc/os-release
 
 # Install dotnet
 # https://docs.microsoft.com/en-us/dotnet/core/install/linux-ubuntu
 # https://learn.microsoft.com/en-us/dotnet/core/install/linux-ubuntu#register-the-microsoft-package-repository
 # https://learn.microsoft.com/en-us/dotnet/core/install/linux-package-mixup?pivots=os-linux-ubuntu#i-need-a-version-of-net-that-isnt-provided-by-my-linux-distribution
-source /etc/os-release
-case $ID in
-debian | ubuntu)
-	wget https://packages.microsoft.com/config/ubuntu/"$UBUNTU_VERSION"/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-	sudo dpkg -i packages-microsoft-prod.deb
-	rm packages-microsoft-prod.deb
-	sudo apt-get update
-	sudo apt-get install -y apt-transport-https &&
-		sudo apt-get update &&
-		sudo apt-get install -y dotnet-sdk-"$DOTNET_VERSION"
-	;;
-mariner)
+case "$ID" in
+"mariner")
 	sudo tdnf install -y dotnet-sdk-"$DOTNET_VERSION"
+	;;
+"ubuntu" | "debian")
+	# Download Microsoft signing key and repository
+	wget https://packages.microsoft.com/config/$ID/$VERSION_ID/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+
+	# Install Microsoft signing key and repository
+	sudo dpkg -i packages-microsoft-prod.deb
+
+	# Clean up
+	rm packages-microsoft-prod.deb
+
+	# Update packages and install
+	sudo apt update
+	sudo apt-get install -y dotnet-sdk-"$DOTNET_VERSION"
+	;;
+*)
+	echo "Unsupported OS: $ID"
+	exit 1
 	;;
 esac
