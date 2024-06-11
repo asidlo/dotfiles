b3b766be437aa9384dd5cd364c5bd76a202f1184
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-06-11 17:09:49 +0000

    Fixes golang install and adds lang

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-curl -L https://go.dev/dl/go"$GO_VERSION".linux-amd64.tar.gz -o /tmp/go.tar.gz
+GO_VERSION=$(curl https://go.dev/VERSION?m=text | head -n1)
+DOWNLOAD_URL="https://dl.google.com/go/$GO_VERSION.linux-amd64.tar.gz"
+curl -L "$DOWNLOAD_URL" -o /tmp/go.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf /tmp/go.tar.gz
 rm /tmp/go.tar.gz
