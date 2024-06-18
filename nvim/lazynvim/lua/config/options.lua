f8774b0cc74606d0170bc7787ea941b0f4308809
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-06-18 19:23:46 +0000

    Forgot to remove from options

--- nvim/lazynvim/lua/config/options.lua
+++ nvim/lazynvim/lua/config/options.lua
@@ -1,6 +1,3 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-if vim.loop.os_uname().sysname == "Windows_NT" then
-  require("nvim-treesitter.install").compilers = { "clang" }
-end
