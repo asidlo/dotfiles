9c51919aab526d7fb254cf77cbbf6fd64af7db4b
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-07-10 09:52:47 -0400

    Removes autoformat and folds for json/xml

--- nvim/lazynvim/after/ftplugin/json.lua
+++ nvim/lazynvim/after/ftplugin/json.lua
@@ -1,5 +1,5 @@
 vim.cmd("setlocal expandtab tabstop=4 shiftwidth=4")
-vim.cmd("setlocal foldlevel=2")
+-- vim.cmd("setlocal foldlevel=2")
 vim.cmd([[ setlocal commentstring=\/\/\ %s ]])
 vim.cmd("setlocal filetype=jsonc")
 vim.cmd("setlocal conceallevel=0")
--- nvim/lazynvim/after/ftplugin/xml.lua
+++ nvim/lazynvim/after/ftplugin/xml.lua
@@ -1,2 +1,3 @@
 vim.cmd("setlocal expandtab tabstop=4 shiftwidth=4")
-vim.cmd("setlocal foldmethod=indent foldlevelstart=999 foldminlines=0")
+vim.cmd("setlocal foldmethod=indent")
+-- vim.cmd("setlocal foldmethod=indent foldlevelstart=999 foldminlines=0")
