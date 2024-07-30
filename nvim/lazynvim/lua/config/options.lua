2130dd81979bcb2c970ef2fde24227a727aa6b90
Andrew Sidlo <andrewsidlo@microsoft.com>
2024-07-30 15:25:27 -0400

    Uses vs dev shell if present

--- nvim/lazynvim/lua/config/options.lua
+++ nvim/lazynvim/lua/config/options.lua
@@ -18,7 +18,7 @@ if vim.loop.os_uname().sysname == "Windows_NT" then
   else
     vim.cmd([[
       let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-      let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+      let &shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';Remove-Alias -Force -ErrorAction SilentlyContinue tee;$vsPath = &(Join-Path 'C:\Program Files (x86)' '\Microsoft Visual Studio\Installer\vswhere.exe') -property installationpath -latest;Import-Module (Join-Path $vsPath 'Common7\Tools\Microsoft.VisualStudio.DevShell.dll');Enter-VsDevShell -VsInstallPath $vsPath -SkipAutomaticLocation;if ([System.IO.File]::Exists('.\AddModules.ps1')) { .\AddModules.ps1 };"
       let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
       let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
       set shellquote= shellxquote=
