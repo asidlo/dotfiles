7ed3c7ab3e24a21ee9bd5c8d51a09f7d1c3dcbbe
Andrew Sidlo <andrewsidlo@microsoft.com>
2025-02-19 18:11:03 +0000

    Adds method for alt linux

--- scripts/pwsh.sh
+++ scripts/pwsh.sh
@@ -9,16 +9,28 @@ fi
 # Install powershell
 case "$ID" in
 ubuntu)
-	sudo apt-get update &&
-		sudo apt-get install -y wget apt-transport-https software-properties-common &&
-		wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb" &&
-		sudo dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb && sudo apt-get update &&
-		sudo apt-get install -y powershell
-	;;
+  sudo apt-get update &&
+    sudo apt-get install -y wget apt-transport-https software-properties-common &&
+    wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb" &&
+    sudo dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb && sudo apt-get update &&
+    sudo apt-get install -y powershell
+  ;;
 *)
-	echo "Unsupported OS"
-	exit 1
-	;;
+  # Download the powershell '.tar.gz' archive
+  curl -L -o /tmp/powershell.tar.gz https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/powershell-7.5.0-linux-x64.tar.gz
+
+  # Create the target folder where powershell will be placed
+  sudo mkdir -p /opt/microsoft/powershell/7
+
+  # Expand powershell to the target folder
+  sudo tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7
+
+  # Set execute permissions
+  sudo chmod +x /opt/microsoft/powershell/7/pwsh
+
+  # Create the symbolic link that points to pwsh
+  sudo ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+  ;;
 esac
 
 mkdir -p ~/.config/powershell
